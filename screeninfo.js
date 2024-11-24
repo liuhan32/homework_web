b5ace33a523b69524ab0edfeb698e44f1fa44ad7
liuhan32 <3497613462@qq.com>
2024-11-24 17:02:04 +0800

    Add files via upload

--- screeninfo.js
+++ screeninfo.js
@@ -27,6 +27,7 @@ const screenData = {
 // 每秒更新一次屏幕信息并显示
 function updateScreenInfo() {
   screenData.updateScreenInfo(); // 更新时间
+console.log(screenData.formatScreenInfo())
   document.getElementById('screen-info').textContent = screenData.formatScreenInfo(); // 更新屏幕信息显示
 }
 
